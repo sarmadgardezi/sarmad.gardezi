bf4a64cc35a107489299bf415da7855fa4709765
Sarmad Gardezi <mail@sarmadgardezi.com>
2022-06-19 17:45:56 +0500

    New

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           spacing={3}
         >
           <Heading as="h1" size="lg">
-            Hi, I’m Lazar Nikolov.
+            Hi, I’m Sarmad Gardezi.
           </Heading>
           <HeroPlay />
         </Stack>
