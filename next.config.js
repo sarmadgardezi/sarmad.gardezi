57dee4d47a4c22f27807fa44822d3b952fe110a3
Sarmad Gardezi <mail@sarmadgardezi.com>
2022-06-26 22:08:59 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,5 @@
-const { withSentryConfig } = require('@sentry/nextjs');
-
-const OLD_BLOG_URL = 'https://hn.nikolovlazar.com';
-const oldBlogPosts = [
-  '/portfolio-beef-1-free-designs-for-beginner-front-end-developers',
-  '/my-2020-retrospective',
-  '/living-with-a-migraine-as-a-software-developer',
-  '/an-easy-react-17-typescript-tailwind-css-nextjs-setup',
-  '/debugging-nextjs',
-  '/improve-your-design-handoff',
-  '/uxcel-ux-ui-for-developers-and-designers',
-  '/how-to-render-your-website',
-];
-
 const moduleExports = {
-  swcMinify: true,
+
   images: {
     domains: [
       'img.youtube.com',
@@ -38,18 +24,3 @@ const moduleExports = {
     ];
   },
 };
-
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  token: process.env.SENTRY_AUTH_TOKEN,
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
